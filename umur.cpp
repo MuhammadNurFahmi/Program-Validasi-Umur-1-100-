c432121ab4fd48590f0768c1312630529d5dacc7
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-17 11:43:50 +0700

    Add files via upload

---
+++ umur.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+
+
+      int umur, thn;
+      char nama [30];
+      cout<<"masukan nama anda = ";
+      cin>> nama;
+      cout<<"masukan tahun lahir anda = ";
+      cin>> thn;
+      umur=2018-thn;
+      cout<<" umur " << nama  <<" saat ini adalah = " <<umur;
+      return 0;
+      }
